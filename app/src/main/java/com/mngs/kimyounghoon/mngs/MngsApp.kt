eb9f5666067d7f229f3e504312d59ded005f4ef8
kimyounghoon <kimyh8496@gmail.com>
2018-11-14 10:35:22 +0900

    add fabric crashlytics

--- app/src/main/java/com/mngs/kimyounghoon/mngs/MngsApp.kt
+++ app/src/main/java/com/mngs/kimyounghoon/mngs/MngsApp.kt
@@ -3,11 +3,14 @@ package com.mngs.kimyounghoon.mngs
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class MngsApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics())
         context = applicationContext
     }
 
